76b1383cf8ae4374e904f9ab760b22bdb3a793e6
Brayan-Gomez-01 <55248083+Brayan-Gomez-01@users.noreply.github.com>
2025-07-03 06:37:35 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+window.onload = function () {
+  document.getElementById("popup").style.display = "flex";
+};
+
+function cerrarPopup() {
+  document.getElementById("popup").style.display = "none";
+}
+
